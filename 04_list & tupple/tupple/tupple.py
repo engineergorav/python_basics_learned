4f1ac6378314f361e48b0833bf79018403149782
Gorav Thakur <engineergorav@gmail.com>
2025-01-17 18:33:32 +0530

    Add files via upload

---
+++ 04_list & tupple/tupple/tupple.py
@@ -0,0 +1,32 @@
+t=(34,2,3,5,6,1,2,3,4,5,6,7)
+print(t)
+
+
+print(t[2])
+
+print(sorted(t))
+
+
+t1=(1,2,3,4)
+a,b,c,d=t1
+print(a,b,c,d)
+
+
+
+
+for i in t1:
+    print(i)
+
+
+
+print(t+t1)
+
+print(t1*4)
+
+
+print(t[1:9])
+
+
+print(t.index(4))
+
+print(t1.count(1))
